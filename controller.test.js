edcf45d2dcff6bb1934cee1f45aec195af2efa00
Vignesh Nagamuthu <vignesh.nagamuthu@rohde-schwarz.com>
2023-10-11 21:47:05 +0800

    final lah final finalest

--- controller.test.js
+++ controller.test.js
@@ -7,13 +7,13 @@ describe("the print function", ()=>{
         jest.clearAllMocks();
     })
 
-    it("should respond with 'Hello world!'", ()=>{
+    it("should respond with 'Hello world! This is vignesh'", ()=>{
         const mockReq = {};
         const mockRes = {
             send:jest.fn()
         }
 
         targetFn(mockReq, mockRes);
-        expect(mockRes.send).toHaveBeenCalledWith("Hello world!"); // If you change this value, the test will fail.
+        expect(mockRes.send).toHaveBeenCalledWith("Hello world! This is vignesh"); // If you change this value, the test will fail.
     })
 })
